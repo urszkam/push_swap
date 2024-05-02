851a7e7ec9d90a9a7f0bb848b837673821205f1f
Urszula Kaminska <ukaminsk@c3r1s1.42warsaw.pl>
2024-05-02 16:10:46 +0200

    initial commit

---
+++ utils/lst_clear.c
@@ -0,0 +1,20 @@
+void	ft_lstdelone(t_list *lst)
+{
+	if (!lst)
+		return ;
+	free(lst);
+}
+
+void	ft_lstclear(t_list **lst)
+{
+	t_list	*temp;
+
+	if (!lst || !(*lst))
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst);
+		(*lst) = temp;
+	}
+}
