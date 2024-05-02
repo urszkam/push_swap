fad885684f35cd4f233d0dfb0147cb36d70993b4
Urszula Kaminska <ukaminsk@c3r1s1.42warsaw.pl>
2024-05-02 16:12:51 +0200

    docs: update functions list

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,13 +1,20 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+
+typedef struct s_list
+{
+	int			n;
+	struct s_list	*next;
+}	t_list;
 
 int     is_correct(char *s);
 int     is_sorted(char **argv);
 int     ft_atoi(char *s);
+int	has_duplicates(char **argv, int len);
 
 size_t  ft_strlen(char *s);
 
